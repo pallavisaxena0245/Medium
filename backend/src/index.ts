aa04466e607e309e56dda3c7421e5a63abfa983e
pallavisaxena0245 <pallavisaxena0245@gmail.com>
2024-11-24 22:13:30 +0530

    added prisma support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,26 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('signup route')
 })
 
+app.post('/api/v1/sigin', (c) => {
+  return c.text('Signin route!')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  const id = c.req.param("id")
+  console.log(id);
+  return c.text('get blog route')
+
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Post blog!')
+})
+
+app.put('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
 export default app
