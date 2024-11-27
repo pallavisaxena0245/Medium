1ee0e704b438f42ae30c3b747c5974d424a5fd9f
pallavisaxena0245 <pallavisaxena0245@gmail.com>
2024-11-27 15:07:25 +0530

    more changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,26 +2,8 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('signup route')
-})
-
-app.post('/api/v1/sigin', (c) => {
-  return c.text('Signin route!')
-})
-
-app.get('/api/v1/blog/:id', (c) => {
-  const id = c.req.param("id")
-  console.log(id);
-  return c.text('get blog route')
-
-})
-
-app.post('/api/v1/blog', (c) => {
-  return c.text('Post blog!')
-})
-
-app.put('/api/v1/blog/:id', (c) => {
+app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
 export default app
